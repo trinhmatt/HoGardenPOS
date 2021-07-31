4ac216bb993dc47a6ba47bff8e4f408a7a242159
vivyliang <vvnl@live.ca>
2021-07-31 18:58:38 -0400

    home page style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,104 @@
 import React, { useState } from "react";
 import database from "../firebase/firebase";
 
-const Home = () => {
+//Style imports
+import { mainStyles } from "../static/css/mainStyles";
+
+//Material ui imports
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+//Carousel import
+import {Carousel} from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
+//Subcomponent imports
+import ElevationScroll from "./subcomponents/ElevationScroll";
+
+//Menu pdf
+import adc1 from '../static/menu/adc-1.jpg';
+import adc2 from '../static/menu/adc-2.jpg';
+import adc3 from '../static/menu/adc-3.jpg';
+
+const Home = (props) => {
+    const styles = mainStyles();
     const [state, setState] = useState({});
     
     const test = () => {
         database.ref("menu").update({test: "hi"})
     }
     return (
-        <div>
-            homepage
-            <button onClick={test}>test</button>
+        <div className={styles.homePage}>
+             {/* Header */}
+             <ElevationScroll {...props}>
+                    <AppBar id='menu-header'>
+                        <Toolbar className={styles.titleBackground}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={6} className={styles.centered}>
+                                    <div className={styles.title}>半島餐廳</div>
+                                </Grid>
+                                <Grid item xs={6} className={styles.titleRightLayout}>
+                                    <div className={styles.titleRight}>HO GARDEN CHINESE RESTAURANT</div>
+                                    <div className={styles.titleRight}>TEL: 905-927-9623</div>
+                                    <div className={styles.titleRight}>{`DINE-IN & TAKE-OUT LLBO`}</div>
+                                </Grid>
+                            </Grid>
+                        </Toolbar>
+                    </AppBar>
+                </ElevationScroll>
+                <Toolbar />
+                <Carousel
+                    autoPlay={true}
+                    swipeable={true}
+                    emulateTouch={true}
+                    showThumbs={false}
+                >
+                    <div className={styles.parallaxBackground}></div>
+                    <div className={styles.parallaxBackground2}></div>
+                    <div className={styles.parallaxBackground3}></div>
+                </Carousel>
+            <Grid container spacing={0} className={styles.mapBackground}>
+                <Grid item xs={6} className={styles.mapouter}>
+                    <div className={styles.gmap_canvas}>
+                        <iframe width="600" height="330" id="gmap_canvas" src={"https://maps.google.com/maps?q=9255%20woodbine%20ave&t=&z=15&ie=UTF8&iwloc=&output=embed"} frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                        <br />
+                        <a href="https://www.embedgooglemap.net">location map for website</a>
+                    </div>
+                </Grid>
+                <Grid item xs={6} className={styles.titleRightLayout}>
+                    <div className={styles.titleRight}>HOURS/營業時間:</div>
+                    <div className={styles.mapSmall}>MONDAY: 8AM-9PM</div>
+                    <div className={styles.mapSmall}>TUESDAY: 8AM-9PM</div>
+                    <div className={styles.mapSmall}>WEDNESDAY: 8AM-9PM</div>
+                    <div className={styles.mapSmall}>THURSDAY: 8AM-9PM</div>
+                    <div className={styles.mapSmall}>FRIDAY: 8AM-9PM</div>
+                    <div className={styles.mapSmall}>SATURDAY: 8AM-9PM</div>
+                    <div className={styles.mapSmall}>SUNDAY: 8AM-9PM</div>
+                </Grid>
+            </Grid>
+            <div className={styles.afterBackground}>
+                <div className={styles.menuTitle}>MENU/菜單</div>
+                <Carousel
+                    autoPlay={true}
+                    centerMode={true}
+                    dynamicHeight={true}
+                    swipeable={true}
+                    emulateTouch={true}
+                    showIndicators={false}
+                >
+                    <div>
+                        <img src={adc1} className={styles.menuImg}/>
+                    </div>
+                    <div>
+                        <img src={adc2} className={styles.menuImg} />
+                    </div>
+                    <div>
+                        <img src={adc3} className={styles.menuImg} />
+                    </div>
+                </Carousel>
+            </div>
         </div>
     )
 }
--- src/static/css/mainStyles.js
+++ src/static/css/mainStyles.js
@@ -1,5 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles';
 
+//Image
+import bkgrnd from '../images/home.jpeg';
+import bkgrnd2 from '../images/lemontea.jpeg';
+import bkgrnd3 from '../images/milktea.jpeg';
+
 const mainStyles = makeStyles(({spacing}) => ({
     fullPage: {
         minWidth: window.screen.width,
@@ -9,6 +14,175 @@ const mainStyles = makeStyles(({spacing}) => ({
         justifyContent:'center', 
         alignItems:'center', 
     },
+    centered: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    homePage: {
+        width: '100vw',
+        overflowX: 'hidden',
+    },
+
+    //title
+    title: {
+        fontFamily: ['"M PLUS Rounded 1c"', 'sans-serif'].join(','),
+        color: '#ffdd7a',
+        //phone
+        '@media (min-width: 375px)': {  
+            fontSize: '40px',
+        },
+        //ipad+
+        '@media (min-width: 760px)': {  
+            fontSize: '80px',
+            letterSpacing: '20px',
+        },
+    },
+    titleBackground: {
+        background: '#000428',
+        background: '-webkit-linear-gradient(to bottom, #004e92, #000428)',  
+        background: 'linear-gradient(to bottom, #004e92, #000428)',
+        //phone
+        '@media (min-width: 375px)': { 
+            height: '15vh',
+        },
+        //ipad+
+        '@media (min-width: 760px)': {
+            height: '10vh',
+        },
+    },
+    titleRightLayout: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        lineHeight: '2em',
+    },
+    titleRight: {
+        fontFamily: ['"M PLUS Rounded 1c"', 'sans-serif'].join(','),
+        color: '#ffdd7a',
+        fontSize: '25px',
+        //phone
+        '@media (min-width: 375px)': {  
+            fontSize: '10px',
+            lineHeight: '1.5em',
+        },
+        //ipad+
+        '@media (min-width: 760px)': {  
+            fontSize: '25px',
+            lineHeight: '1em',
+        },
+    },
+    menuTitle: {
+        fontFamily: ['"M PLUS Rounded 1c"', 'sans-serif'].join(','),
+        color: '#ffdd7a',
+        display: 'flex',
+        justifyContent: 'center',
+        fontWeight: '500',
+        //phone
+        '@media (min-width: 375px)': {  
+            fontSize: '35px',
+        },
+        //ipad+
+        '@media (min-width: 760px)': {  
+            fontSize: '60px',
+        },
+    },
+
+    //parallax
+    parallaxBackground: {
+        backgroundImage: `url(${bkgrnd})`,
+        minHeight: '600px',
+        minWidth: '160vw',
+        backgroundAttachment: 'fixed',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+    },
+    parallaxBackground2: {
+        backgroundImage: `url(${bkgrnd2})`,
+        minHeight: '600px',
+        minWidth: '100vw',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+    },
+    parallaxBackground3: {
+        backgroundImage: `url(${bkgrnd3})`,
+        minHeight: '600px',
+        minWidth: '100vw',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+    },
+    afterBackground: {
+        padding: '5%',
+        background: '#005C97', 
+        background: '-webkit-linear-gradient(to bottom, #363795, #005C97)', 
+        background: 'linear-gradient(to bottom, #363795, #005C97)', 
+        //phone
+        '@media (min-width: 375px)': { 
+            minHeight: '700px',
+        },
+        //ipad+
+        '@media (min-width: 760px)': { 
+            minHeight: '2000px',
+        },
+    },
+
+    //map
+    mapouter: {
+        position: 'relative',
+        textAlign: 'right',
+        padding: '0 !important',
+        //phone
+        '@media (min-width: 375px)': {  
+            height: '130px',
+            width: '200px',
+        },
+        //ipad+
+        '@media (min-width: 760px)': {  
+            height: '330px',
+            width: '600px',
+        },
+    },
+    gmap_canvas: {
+        overflow: 'hidden',
+        background: 'none!important',
+        //phone
+        '@media (min-width: 375px)': {  
+            height: '130px',
+            width: '200px',
+        },
+        //ipad+
+        '@media (min-width: 760px)': {  
+            height: '330px',
+            width: '600px',
+        },
+    },
+    mapBackground: {
+        width: '100vw',
+        backgroundColor: '#000',
+        padding: '2%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+    },
+    mapSmall: {
+        fontFamily: ['"M PLUS Rounded 1c"', 'sans-serif'].join(','),
+        color: '#ffdd7a',
+        lineHeight: '1.5em',
+        //phone
+        '@media (min-width: 375px)': {  
+            fontSize: '10px',
+        },
+        //ipad+
+        '@media (min-width: 760px)': { 
+            fontSize: '15px',
+        },
+    },
+
+    //menu
 }))
 
 export { mainStyles };
